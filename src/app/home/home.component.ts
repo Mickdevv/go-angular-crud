1f21902d0ac92f96e12bb0369f75f866a3388e2f
Michael <michael@devereux.fm>
2024-11-07 09:03:18 +0100

    Added edit and delete buttons to items cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,12 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { fetchItems } from '../state/items/items.actions';
 import { Store } from '@ngrx/store';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CardModule, CheckboxModule, FormsModule],
+  imports: [CommonModule, CardModule, CheckboxModule, FormsModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
