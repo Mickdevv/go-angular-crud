801af93af9fd69c3490285081df4e026275c6297
Michael <michael@devereux.fm>
2024-11-13 23:45:42 +0100

    token persists after refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component';
 import { Store } from '@ngrx/store';
 import { CookieService } from 'ngx-cookie-service';
-
+import { jwtDecode } from "jwt-decode";
+import { login } from './state/user/user.actions';
+import { UserWithToken } from './models/user.model';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -20,7 +22,9 @@ export class AppComponent {
   ngOnInit() {
     if (this.cookieService.check('jwt_token')) {
       const initialToken = this.cookieService.get('jwt_token')
-      console.warn(initialToken)
+      const decodedToken = jwtDecode(initialToken) as any
+      const user: UserWithToken = { username: decodedToken.username, access: initialToken, refresh: initialToken }
+      this.store.dispatch(login.success({ user }))
     }
   }
 }
